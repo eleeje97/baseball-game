20a4c9d5a0d8e8c7516df5d1fe71c7d985e64688
danalee <eleeje97@gmail.com>
2023-03-17 10:06:16 +0900

    Refactor(BaseBallNumber): 객체 간 올바른 비교를 위해 equals(), hasCode() 메소드 오버라이드

--- src/main/java/org/example/domain/BaseBallNumber.java
+++ src/main/java/org/example/domain/BaseBallNumber.java
@@ -1,5 +1,7 @@
 package org.example.domain;
 
+import java.util.Objects;
+
 import static org.example.domain.GameManager.*;
 
 public class BaseBallNumber {
@@ -27,5 +29,18 @@ public class BaseBallNumber {
 
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseBallNumber that = (BaseBallNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
