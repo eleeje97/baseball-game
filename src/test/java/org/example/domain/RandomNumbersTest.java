c6d29b8b003d76ce1d7e9a5e01ce02389cf3fb44
danalee <eleeje97@gmail.com>
2023-03-22 10:20:11 +0900

    Refactor: 랜덤숫자 생성 테스트 - 중복, 자릿수 테스트 분리

--- src/test/java/org/example/domain/RandomNumbersTest.java
+++ src/test/java/org/example/domain/RandomNumbersTest.java
@@ -1,6 +1,5 @@
 package org.example.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,15 +7,36 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RandomNumbersTest {
-    @DisplayName("중복없이 3자리 수 생성되는지 테스트")
+    @DisplayName("중복없이 생성되는지 테스트")
     @Test
-    void 랜덤숫자_생성_테스트() {
-        RandomNumbers randomNumbers = new RandomNumbers();
-        List<Integer> randomNumberList = randomNumbers.getNumbers();
+    void 랜덤숫자_생성_테스트_중복() {
+        // Given & When
+        RandomNumbersInterface randomNumbers = new RandomNumbers();
 
-        Set<Integer> randomNumberSet = new HashSet<>(randomNumberList);
+        // Then
+        assertThat(isNotExistsSameNumbers(randomNumbers.getNumbers())).isTrue();
+    }
 
-        Assertions.assertThat(randomNumberSet.size()).isEqualTo(3);
+    @DisplayName("3자리 수 생성되는지 테스트")
+    @Test
+    void 랜덤숫자_생성_테스트_자릿수() {
+        // Given & When
+        RandomNumbersInterface randomNumbers = new RandomNumbers();
+
+        // Then
+        assertThat(hasThreeNumbers(randomNumbers.getNumbers())).isTrue();
+    }
+
+
+    private boolean isNotExistsSameNumbers(List<Integer> numbers) {
+        Set<Integer> randomNumberSet = new HashSet<>(numbers);
+        return randomNumberSet.size() == numbers.size();
+    }
+
+    private boolean hasThreeNumbers(List<Integer> numbers) {
+        return numbers.size() == 3;
     }
 }
